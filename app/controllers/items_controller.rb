6134b242f849c70e0eafd3dc7f0a631c93d45bef
vagrant <vagrant@localhost.localdomain>
2020-07-18 09:23:15 +0000

    [Update]item_show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,18 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+   def create
+   	@item = Item.find(params[:id])
+    cart_item = current_member.cart_items.new(cart_item_params)
+    cart.save
+    redirect_to request.referer
+  end
+
+   private
+  def cart_item_params
+    params.require(:cart_item).permit(:count)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
   sessions: 'admins/sessions'
   }
+  
   devise_for :members
   root :to => 'homes#top'
   get 'homes/about', to: 'homes#about'
@@ -19,10 +20,10 @@ Rails.application.routes.draw do
 
   resources :deliveries, only: [:index, :create, :edit, :destroy, :update]
 
-  resources :carts, only: [:create, :index, :update, :destroy]
+  resources :items, only: [:index, :show, :create] do 
+      resources :carts, only: [:create, :index, :update, :destroy]
+  end
 
-
-  resources :items, only: [:index, :show]
   resources :oreders
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
