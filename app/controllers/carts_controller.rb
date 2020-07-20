cf90e87bc937ab545de085802f4c2d8f3b673181
vagrant <vagrant@localhost.localdomain>
2020-07-20 06:43:27 +0000

    [Update]cart_index

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,8 @@ class CartsController < ApplicationController
   def create
     cart_item = CartItem.find_by(member_id: current_member.id, item_id: params[:cart_item][:item_id])
 
-    if  cart_item　# cart_item が存在する場合の意味（.present を省略している）
+    if  cart_item
+    # cart_item が存在する場合の意味（.present を省略している）
         cart_item.count += params[:cart_item][:count].to_i
       else
         cart_item = CartItem.new(cart_item_params)
@@ -15,9 +16,16 @@ class CartsController < ApplicationController
 
   def index
     @cart_items = CartItem.all
+    cart_item = CartItem.find(id: cart_item.id)
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
   end
 
   def destroy
