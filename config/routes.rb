9e74566c4f4cfcdb0a325644ba8f1490edd07eaf
vagrant <vagrant@localhost.localdomain>
2020-07-23 07:34:17 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -29,8 +29,9 @@ Rails.application.routes.draw do
 
   resources :deliveries, only: [:index, :create, :edit, :destroy, :update]
 
-  get 'orders/:id/confirm', to: 'orders#confirm', as: 'order_confirm'
-  post 'orders/:id/confirm', to: 'orders#post', as: 'order_confirm_post'
+  get 'orders/confirm', to: 'orders#confirm', as: 'order_confirm'
+  post 'orders/confirm', to: 'orders#confirm', as: 'order_confirm_post'
+  post 'orders/create', to: 'orders#create', as: 'order_create'
   get 'orders/done', to: 'orders#done'
   resources :orders
 
