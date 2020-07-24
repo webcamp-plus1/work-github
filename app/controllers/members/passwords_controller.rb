1c497b41a44ad21471d12e1af51f3e0f5aa3ad1a
vagrant <vagrant@localhost.localdomain>
2020-07-24 05:28:48 +0000

    [Fix]css

--- app/controllers/members/passwords_controller.rb
+++ app/controllers/members/passwords_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Members::PasswordsController < Devise::PasswordsController
+
   # GET /resource/password/new
   # def new
   #   super
@@ -31,4 +32,5 @@ class Members::PasswordsController < Devise::PasswordsController
   # def after_sending_reset_password_instructions_path_for(resource_name)
   #   super(resource_name)
   # end
+
 end
