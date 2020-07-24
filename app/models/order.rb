cc016aa2485613d32b9f942e2c26505e4cd5a3a7
Owner <owner@MA4-0029.local>
2020-07-24 19:46:16 +0900

    [order/index]

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :member
-  has_many :order_item, dependent: :destroy
+  has_many :order_items, dependent: :destroy
 
   enum payment_method: {card: 1, bank: 2 }
 
