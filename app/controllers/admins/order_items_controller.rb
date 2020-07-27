166b63bd61693c3fcc4ff2c2c1a37c91367cc1a7
vagrant <vagrant@localhost.localdomain>
2020-07-27 11:34:57 +0000

    [Update]auto_load

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -3,8 +3,16 @@ class Admins::OrderItemsController < Admins::OrdersController
 	def update
     @order_item = OrderItem.find(params[:id])
 	  if @order_item.update(order_item_params)
-	    redirect_to admins_orders_path
-	  else
+	  	if @order_item.production_status == 'in_production'
+	  		@order_item.order.update(status: "in_production")
+	  		# @order_item.order.status = 'in_production'
+	  		# @order_item.order.update
+	  	elsif @order_item.order.order_items.pluck(:production_status).all? {|production_status| production_status == 'done'}
+	  		# .pluck(:カラム名)でカラムの値を全て取得。その後'done'を探す １つでも違うとfalseを返す
+	  		@order_item.order.update(status: 'preparing')
+	  		# @order_item.order.status = 'preparing'
+	  		# @order_item.order.update
+	  	end
 	    redirect_to request.referer
 	  end
   end
