29c287c62fdbd4cd5ac1403421f203b17f901fc6
Owner <owner@MA4-0029.local>
2020-07-27 11:12:01 +0900

    error_message

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,10 @@
 class Delivery < ApplicationRecord
   belongs_to :member
 
+  validates :postal_code, presence: true
+  validates :destination, presence: true
+  validates :addressee, presence: true
+
 	def ffff
 		postal_code + destination + addressee
 	end
